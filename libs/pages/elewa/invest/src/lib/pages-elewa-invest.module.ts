0559e2bfae837b595631e410d587853d76b36e1a
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-02-27 19:39:43 +0300

    🎨 feat: imported banner module

--- libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
+++ libs/pages/elewa/invest/src/lib/pages-elewa-invest.module.ts
@@ -4,9 +4,14 @@ import { InvestPageComponent } from './pages/invest-page/invest-page.component';
 
 import { InvestRoutingModule } from './invest.routing';
 import { ElewaInvestDetailSectionComponent } from './components/elewa-invest-detail-section/elewa-invest-detail-section.component';
-
+import { ElewaInvestInvestingInElewaSectionComponent } from './components/elewa-invest-investing-in-elewa-section/elewa-invest-investing-in-elewa-section.component';
+import { BannersModule } from '@elewa-group/features/components/banners';
 @NgModule({
-  imports: [CommonModule, InvestRoutingModule],
-  declarations: [InvestPageComponent, ElewaInvestDetailSectionComponent],
+  imports: [CommonModule, InvestRoutingModule, BannersModule],
+  declarations: [
+    InvestPageComponent,
+    ElewaInvestDetailSectionComponent,
+    ElewaInvestInvestingInElewaSectionComponent,
+  ],
 })
 export class InvestPageModule {}
