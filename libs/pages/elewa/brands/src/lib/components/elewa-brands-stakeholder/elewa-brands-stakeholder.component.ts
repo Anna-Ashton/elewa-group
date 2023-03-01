ddb3fcfcbe3aec5745f53c4806e8c0afc4a38b17
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-03-01 14:49:16 +0300

    🎨 feat:  stakeholders appearance

---
+++ libs/pages/elewa/brands/src/lib/components/elewa-brands-stakeholder/elewa-brands-stakeholder.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-elewa-brands-stakeholder',
+  templateUrl: './elewa-brands-stakeholder.component.html',
+  styleUrls: ['./elewa-brands-stakeholder.component.scss'],
+})
+export class ElewaBrandsStakeholderComponent {
+ 
+}
