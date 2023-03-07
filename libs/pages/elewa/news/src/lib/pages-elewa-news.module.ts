2e22c4357cd931b7cdae9a8148f55544ec20b35c
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-03-07 10:43:58 +0300

    🎨 feat: import module UIList

--- libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
+++ libs/pages/elewa/news/src/lib/pages-elewa-news.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiListsModule } from '@elewa-group/features/components/ui-lists'
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 
 import { NewsRoutingModule } from './news.routing';
 
 @NgModule({
-  imports: [CommonModule, NewsRoutingModule],
+  imports: [CommonModule, NewsRoutingModule, UiListsModule],
   declarations: [NewsPageComponent],
 })
 export class NewsPageModule {}
